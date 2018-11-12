8d5286ff2d0f0816aa7c986f76ae3db5add7f568
青虫 <qingchong@dian.so>
2018-11-12 18:14:55 +0800

    fix bug in new project

--- src/download.js
+++ src/download.js
@@ -1,8 +1,10 @@
 const download = require('download-git-repo')
 const config = require('./config')
+const insertConfig = require('./insertConfig')
 module.exports = function(target, type) {
+  const temList = {...config.TEM_LIST, ...insertConfig.TEM_LIST}
   return new Promise((resolve, reject) => {
-    download(config.TEM_LIST[type].addr, target, {clone: true}, err => {
+    download(temList.TEM_LIST[type].addr, target, {clone: true}, err => {
       if (err) {
         reject(err)
       } else {
